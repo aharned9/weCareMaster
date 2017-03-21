aa6b1a7a37413b7ea1c25afe6a8bc311f5cc2f76
aharned9 <arharned@my.waketech.edu>
2017-03-21 19:43:40 -0400

    Add files via upload

--- AddNewChild.php
+++ AddNewChild.php
@@ -1,24 +1,30 @@
 <!DOCTYPE HTML>
 <html lang = "en">
   <head>
-    <title>WeCareWorkerLanding.html</title>
+    <title>Admin - Edit Child</title>
     <meta charset = "UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="css/bootstrap.css" rel="stylesheet">
 	<script>
 	function deleteCheck(){
 		return confirm('Are you sure you want to delete this entry?');
-			
 	}
 	</script>
 
 	<style>
-		label, input{margin-bottom: 20px;}
+		label{color: white; margin-bottom: 20px;}
+		input{color: black; margin-bottom: 20px;}
 		html, body{position:relative; height: 100%}
 		div{ height: 100%}
+		h5 {color: #b3ffb3}
 		#names{border: 1px solid black}
-		th, td {padding: 10px}
+		th {color: white}
+		td {padding: 10px}
 		#topPage{ height: 100%; padding: 10px;}
 		.left{ width: 20%; float: left; text-align: right:}
 		.right{ width: 70%; margin-left: 10px; float:left;}
+		.black(font-color: black)
 		
 		ul { list-style-type: none; margin: 0; padding: 0; overflow: hidden; background-color: #333;}
 		li {float: left;}
@@ -65,22 +71,24 @@
 	</style>
   </head>
   <body>
-    <h1><span>We</span>Care</h1>
-	<ul>
-		<li><a href="ChildDailyAct.php">Home</a></li>
-		<li><a href="AddNewChild.php">Add Child</a></li>
+	
+    <h1><span><img src="img/wecare.png" class="img-rounded" alt="daycare-image" width="75" height="75">We</span>Care - Administrator: Edit Child Database</h1>
+	<ul class="nav nav-pills nav-justified">
+		<li><a href="Workerlandingpage.php">Home</a></li>
+		<li class="active"><a href="AddNewChild.php">Add Child</a></li>
 		<li><a href="AddNewWorker.php">Add Employee</a></li>
 		<li><a href="PasswordReset.php">Password Reset</a></li>
 	</ul>
-	<div id="names" style="width: 200px;background-color: #FFFFCC; float: left;">
-	<form action="AddNewChild.php" method="get">
-		<input type="submit" value="Add New" id="new">
-		
-		
-	</form>
-		<table rules="rows" width="200px">
+	<div class="table-responsive" id="names" style="width: 200px;background-color: #1a1a1a; float: left;">
+			<table class="table" width="200px">
 			<tr>
-				<th><center>Name</center></th>
+				<th><center>
+				<h4>Child Roster</h4>
+				<a href="AddNewChild.php" class="btn btn-primary" role="button">Add New</a>
+				<br />
+				</th>
+			</tr>	
+			
 				
 <?php
 $server = "localhost";
@@ -100,6 +108,7 @@ while ($ChildRoster = mysqli_fetch_assoc($result))
 ?>
 		</table>
 	</div>
+	
 
 	
 <?php
@@ -122,96 +131,114 @@ if (empty($ChildKey) && empty($result)){
 		echo "<meta http-equiv='refresh' content='0'>";
 	}
 ?>
-	<div id="topPage" style="margin-left: 200px; background-color: #CCFFFF;" valign="top">
-	<b>Please Add a new child, or select a child to Edit/Delete</b>
-	<p></p>
-
+	
+	<div class="table-responsive" id="topPage" style="margin-left: 200px; background-color: #006600;" valign="top">
+	<h5><center>Please Add a new child, or select a child to Edit/Delete</center></h5>
+	
+	
  <!-- columns divs, float left, no margin so there is no space between column, width=1/3 -->
-    <form method="post">
+    
+	<form method="post">
+			<div class="container-fluid">
+			<div class="row table-responsive">
+				<div class="col-xs-7">
 			
-			<div class="left">
+			<div class="col-sm-4">
 				<label for="name">Child First Name</label>
 			</div>
-			<div class="right">
-				<input type = "text" size="16" name = "fName" value="">
+			<div class="col-sm-8">
+				<input type = "text" size="25" name = "fName" value="">
 			</div>
 			
 			
-			<div class="left">
+						
+			<div class="col-sm-4">
 				<label for="name">Child Last Name</label>
 			</div>
-			<div class="right">
-				<input type = "text" size="16" name = "lName" value="" >
+			<div class="col-sm-8">
+				<input type = "text" size="25" name = "lName" value="" >
 			</div> 
-			
-            
-			<div class="left">
+				
+			           
+			<div class="col-sm-4">
 				<label for="street">Street</label>
-			</div> 
-			<div class="right">
-				<input type = "text" size="16" name = "childStreet" value="" >
-			</div><
+			</div>
+			<div class="col-sm-8">
+				<input type = "text" size="25" name = "childStreet" value="" >
+			</div>
                   
-			<div class="left">
+			<div class="col-sm-4">
 				<label for="city">City</label> 
 			</div>
-			<div class="right">
-				<input type = "text" size="16" name = "childCity" value="">
+			<div class="col-sm-8">	
+				<input type = "text" size="25" name = "childCity" value="">
 			</div>
 			
-			<div class="left">
+			<div class="col-sm-4">
 				<label for="state">State</label>
-			</div><div class="right">
-				<input type = "text" name = "childState" size="16" value="" >
+			</div>
+			<div class="col-sm-8">
+				<input type = "text" size ="4" name = "childState" value="" >
 			</div>
 			
-			<div class="left">
+			<div class="col-sm-4">
 				<label for="zip">Zip</label>
-			</div><div class="right">
-				<input type = "text" name = "childZip" size="16" value="">
+			</div>
+			<div class="col-sm-8">
+				<input type = "text" size="5" name = "childZip" value="">
 			</div>
 			
-			<div class="left">
+			<div class="col-sm-4">
 				<label for="Allergies" >Allergies</label>
-			</div> <div class="right">
-				<input type = "text" name = "childAllergies" size="16" value="" > 
+			</div>
+			<div class="col-sm-8">
+				<input type = "text" size="25" name = "childAllergies" value="" > 
 			</div>
 			
 			
                  
-			<div class="left">
+			<div class="col-sm-4">
 				<label for="ParentName">Parent Name</label>
-			</div><div class="right">
-				<input type = "text" name = "childParent" size="16" value=""  >
+			</div>
+			<div class="col-sm-8">
+				<input type = "text" size="25" name="childParent" value=""  >
 			</div>
 			
-			<div class="left">           
+			<div class="col-sm-4">           
 				<label for="ParentContact">Parent Email</label>
-			</div> <div class="right">
-				<input type = "text" name = "childParentEmail" size="16" value="" >
+			</div>
+			<div class="col-sm-8">
+				<input type = "text" size="25" name = "childParentEmail" value="" >
 			</div>
                   
-			<div class="left">
+			<div class="col-sm-4">
 				<label for="ParentNum" >Parent Number</label>
-			</div><div class="right">
-				<input type = "text" name = "childParentNum" size="16" value="" >
+			</div>
+			<div class="col-sm-8">
+				<input type = "text" size="13" name = "childParentNum" value="" >
 			</div>
 			
-			<div class="left">
+			<div class="col-sm-4">
 				<label for="SpecialInstructions" >Special Instructions</label>
-			</div><div class="right">
-				<input type = "text" name = "specialInstruct" size="50" value="" >
+			</div>
+			<div class="col-sm-8">
+				<input type = "text" size="25" name = "specialInstruct" value="" >
 			</div>
 				
-		
-		<div class="left">
-			<button type="submit" name="save">Save</button>
+			</div>
+			
+			<div class="col-xs-5"><br /><br /><img src="img/wecare.png" class="img-rounded" alt="daycare-image" width="85%" height="auto"></div>
+			
+			</div>
+		<div class="right">
+			<button type="submit" class="btn btn-success" name="save">Save New Child</button>
+		</div>	
 	</form>
-	<form action="ChildDailyAct.php">
-		</div> <div class="right">
-			<button type="submit" name="back">Back</button>
-			</div></div>
-	</form>
+	</div>
+	
+	
+	
+	</div>
 		
 <?php
 }
@@ -257,102 +284,124 @@ while($ChildInfo = mysqli_fetch_assoc($result))
 		}
 ?>
 
-<div id="topPage" style="margin-left: 200px; background-color: #CCFFFF;" valign="top">
+<div class="row" id="topPage" style="margin-left: 200px; background-color: #006600;" valign="top">
 
 
  <!-- columns divs, float left, no margin so there is no space between column, width=1/3 -->
     <form method="post">
+			<div class="container-fluid">
+			<div class="row  table-responsive">
+				<div class="col-xs-7">
 			
-			<div class="left">
-				<label for="name">Child First Name</label>
-			</div>
-			<div class="right">
-				<?php echo '<input type = "text" size="16" name = "fName" value="' . $ChildInfo['First_Name'] . '">'?>
-			</div>
-			
-			
-			<div class="left">
-				<label for="name">Child Last Name</label>
-			</div>
-			<div class="right">
-				<?php echo '<input type = "text" size="16" name = "lName" value="' . $ChildInfo['Last_Name'] . '" >'?>
-			</div>
-			
-            
-			<div class="left">
-				<label for="street">Street</label>
-			</div> 
-			<div class="right">
-				<?php echo '<input type = "text" size="16" name = "childStreet" value="' . $ChildInfo['Street'] . '" >'?>
-			</div>
-                  
-			<div class="left">
-				<label for="city">City</label> 
-			</div>
-			<div class="right">
-				<?php echo '<input type = "text" size="16" name = "childCity" value="' . $ChildInfo['City'] . '">' ?>
-			</div>
-			
-			<div class="left">
-				<label for="state">State</label>
-			</div><div class="right">
-				<?php echo '<input type = "text" name = "childState" size="16" value="' . $ChildInfo['State'] . '" >' ?>
-			</div>
-			
-			<div class="left">
-				<label for="zip">Zip</label>
-			</div><div class="right">
-				<?php echo '<input type = "text" name = "childZip" size="16" value="' . $ChildInfo['Zip'] . ' ">' ?>
-			</div>
-			
-			<div class="left">
-				<label for="Allergies" >Allergies</label>
-			</div> <div class="right">
-				<?php echo '<input type = "text" name = "childAllergies" size="16" value="' . $ChildInfo['Allergies'] . '" > ' ?>
-			</div>
+					<div class="col-sm-4">
+						<label for="name">Child First Name</label>
+					</div>
+					<div class="col-sm-8">
+						<?php echo '<input type = "text" size="25" name = "fName" value="' . $ChildInfo['First_Name'] . '">'?>
+					</div>
 			
 			
-                 
-			<div class="left">
-				<label for="ParentName">Parent Name</label>
-			</div><div class="right">
-				<?php echo '<input type = "text" name = "childParent" size="16" value="' . $ChildInfo['Parent_Name'] . '"  >' ?>
-			</div>
-			
-			<div class="left">           
-				<label for="ParentContact">Parent Email</label>
-			</div> <div class="right">
-				<?php echo '<input type = "text" name = "childParentEmail" size="16" value="' . $ChildInfo['Parent_Email'] . '" >' ?>
-			</div>
-                  
-			<div class="left">
-				<label for="ParentNum" >Parent Number</label>
-			</div><div class="right">
-				<?php echo '<input type = "text" name = "childParentNum" size="16" value="' . $ChildInfo['Parent_Phone_Num'] . '" >' ?>
-			</div>
-			
-			<div class="left">
-				<label for="SpecialInstructions" >Special Instructions</label>
-			</div><div class="right">
-				<?php echo '<input type = "text" name = "specialInstruct" size="50" value="' . $ChildInfo['Special_Instructions'] . '" >' ?>
-			</div>
+					<div class="col-sm-4">
+						<label for="name">Child Last Name</label>
+					</div>
+					<div class="col-sm-8">
+						<?php echo '<input type = "text" size="25" name = "lName" value="' . $ChildInfo['Last_Name'] . '" >'?>
+					</div>
 					
+					
+					<div class="col-sm-4">
+						<label for="street">Street</label>
+					</div> 
+					<div class="col-sm-8">
+						<?php echo '<input type = "text" size="25" name = "childStreet" value="' . $ChildInfo['Street'] . '" >'?>
+					</div>
+						  
+					<div class="col-sm-4">
+						<label for="city">City</label> 
+					</div>
+					<div class="col-sm-8">
+						<?php echo '<input type = "text" size="25" name = "childCity" value="' . $ChildInfo['City'] . '">' ?>
+					</div>
+					
+					<div class="col-sm-4">
+						<label for="state">State</label>
+					</div>
+					<div class="col-sm-8">
+						<?php echo '<input type = "text"  size="4" name="childState" value="' . $ChildInfo['State'] . '" >' ?>
+					</div>
+					
+					<div class="col-sm-4">
+						<label for="zip">Zip</label>
+					</div>
+					<div class="col-sm-8">
+						<?php echo '<input type = "text" size="5" name="childZip" value="' . $ChildInfo['Zip'] . ' ">' ?>
+					</div>
+					
+					<div class="col-sm-4">
+						<label for="Allergies" >Allergies</label>
+					</div> 
+					<div class="col-sm-8">
+						<?php echo '<input type = "text" name = "childAllergies" size="25" value="' . $ChildInfo['Allergies'] . '" > ' ?>
+					</div>
+					
+					
+						 
+					<div class="col-sm-4">
+						<label for="ParentName">Parent Name</label>
+					</div>
+					<div class="col-sm-8">
+						<?php echo '<input type = "text" name = "childParent" size="25" value="' . $ChildInfo['Parent_Name'] . '"  >' ?>
+					</div>
+					
+					<div class="col-sm-4">           
+						<label for="ParentContact">Parent Email</label>
+					</div>
+					<div class="col-sm-8">
+						<?php echo '<input type = "text" name = "childParentEmail" size="25" value="' . $ChildInfo['Parent_Email'] . '" >' ?>
+					</div>
+						  
+					<div class="col-sm-4">
+						<label for="ParentNum" >Parent Number</label>
+					</div>
+					<div class="col-sm-8">
+						<?php echo '<input type = "text" name = "childParentNum" size="13" value="' . $ChildInfo['Parent_Phone_Num'] . '" >' ?>
+					</div>
+					
+					<div class="col-sm-4">
+						<label for="SpecialInstructions" >Special Instructions</label>
+					</div>
+					<div class="col-sm-8">
+						<?php echo '<input type = "text" name = "specialInstruct" size="25" value="' . $ChildInfo['Special_Instructions'] . '" >' ?>
+												
+					</div>
+					
+				</div>
+			<div class="col-xs-5"><br /><br /><img src="img/wecare.png" class="img-rounded" alt="daycare-image" width="85%" height="auto"></div>
+		
+		</div>
 		
 		
 		<div class="left">
 		
-		<button type="submit" name="update">Update</button>
+		<button type="submit" class="btn btn-success" name="update">Update</button>
 		
 		
 		
 		</div> <div class="right">
-		<button type="submit" name="delete" onclick="return deleteCheck()" >Delete</button>
-	
-		
+		<button type="submit" class="btn btn-danger" name="delete" onclick="return deleteCheck()" >Delete</button>
+		</div>
+		</div>
+				
 		
 		</form>
+		</div>
 	<?php ;}
 }
 ?>
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+	</div>
   </body>
 </html>
--- AddNewWorker.php
+++ AddNewWorker.php
@@ -1,72 +1,92 @@
 <!DOCTYPE HTML>
 <html lang = "en">
   <head>
-    <title>NewEmployee.html</title>
+    <title>Admin - Edit Employee</title>
     <meta charset = "UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="css/bootstrap.css" rel="stylesheet">
 	<script>
 	function deleteCheck(){
 		return confirm('Are you sure you want to delete this entry?');
 	}
 	</script>
+	
 	<style>
-	label, input{margin-bottom: 20px;}
-	html, body{position:relative; height: 100%}
-	div{ height: 100%}
-	#names{border: 1px solid black}
-	th, td {padding: 10px}
-	#topPage{ height: 100%; padding: 10px;}
-	.left{ width: 20%; float: left; text-align: right:}
-	.right{ width: 70%; margin-left: 10px; float:left;}
-	
-	ul { list-style-type: none; margin: 0; padding: 0; overflow: hidden; background-color: #333;}
-	li {float: left;}
-	li a {display: inline-block; color: white; text-align: center; padding: 14px 16px; text-decoration: none;}
-	li a:hover { background-color: #111;}
-	span{ color:red}
-	#topPage{ height: 100%; padding: 10px;}
-	.left{ width: 20%; float: left; text-align: right:}
-	.right{ width: 70%; margin-left: 10px; float:left;}
-	#Save {
-	display: block;
-	font-size: 1.1em;
-	font-weight: bold;	
-	padding: 5px 15px;
-	margin: 20px auto;
-	width: 75px;
-    background-color: #555;
-	color: #ccc;
-	background: -webkit-linear-gradient(#888, #555);
-	background: linear-gradient(#888, #555);
-	text-shadow: 0 -1px 0 #000;
-	box-shadow: 0 1px 0 #666, 0 5px 0 #444, 0 6px 6px rgba(0,0,0,0.6);
-	cursor: pointer;}
-	#new {
-	display: block;	font-size: 1.1em; font-weight: bold; padding: 5px 5px;
-	margin: 20px auto; margin-top: .5cm; width: 100px; background-color: #555;
-	color: #ccc; background: -webkit-linear-gradient(#888, #555); background: linear-gradient(#888, #555);
-	text-shadow: 0 -1px 0 #000;	box-shadow: 0 1px 0 #666, 0 5px 0 #444, 0 6px 6px rgba(0,0,0,0.6);
-	cursor: pointer; left: 40px; text-align: center;	}
-
-	
+		label{color: white; margin-bottom: 20px;}
+		input{color: black; margin-bottom: 20px;}
+		html, body{position:relative; height: 100%}
+		div{ height: 100%}
+		h5 {color: #b3ffb3}
+		#names{border: 1px solid black}
+		th {color: white}
+		td {padding: 10px}
+		#topPage{ height: 100%; padding: 10px;}
+		.left{ width: 20%; float: left; text-align: right:}
+		.right{ width: 70%; margin-left: 10px; float:left;}
+		.black(font-color: black)
+		
+		ul { list-style-type: none; margin: 0; padding: 0; overflow: hidden; background-color: #333;}
+		li {float: left;}
+		li a {display: inline-block; color: white; text-align: center; padding: 14px 16px; text-decoration: none;}
+		li a:hover { background-color: #111;}
+		span{ color:red}
+		#Save1 {
+		display: block;
+		font-size: 1.1em;
+		font-weight: bold;
+		text-align: center;		
+		padding: 5px 0px;		
+		width: 10%;
+		background-color: #555;
+		color: #ccc;
+		background: -webkit-linear-gradient(#888, #555);
+		background: linear-gradient(#888, #555);
+		text-shadow: 0 -1px 0 #000;
+		box-shadow: 0 1px 0 #666, 0 5px 0 #444, 0 6px 6px rgba(0,0,0,0.6);
+		cursor: pointer;}
+		#new {
+		display: block;	font-size: 1.1em; font-weight: bold; padding: 5px 5px;
+		margin: 20px auto; margin-top: .5cm; width: 100px; background-color: #555;
+		color: #ccc; background: -webkit-linear-gradient(#888, #555); background: linear-gradient(#888, #555);
+		text-shadow: 0 -1px 0 #000;	box-shadow: 0 1px 0 #666, 0 5px 0 #444, 0 6px 6px rgba(0,0,0,0.6);
+		cursor: pointer; left: 40px; text-align: center;	}
+		
+		#trial {
+		display: block;
+		font-size: 1.1em;
+		text-align: left;
+		font-weight: bold;	
+		padding: 5px 45px;		
+		width: 5%;
+		background-color: #555;
+		color: #ccc;
+		background: -webkit-linear-gradient(#888, #555);
+		background: linear-gradient(#888, #555);
+		text-shadow: 0 -1px 0 #000;
+		box-shadow: 0 1px 0 #666, 0 5px 0 #444, 0 6px 6px rgba(0,0,0,0.6);
+		cursor: pointer;
+		}
+} 
 	</style>
   </head>
   <body>
-    <h1><span>We</span>Care</h1>
-	<ul>
-		<li><a href="ChildDailyAct.php">Home</a></li>
+    <h1><span><img src="img/wecare.png" class="img-rounded" alt="daycare-image" width="75" height="75">We</span>Care - Administrator: Edit Employee Database</h1>
+	<ul class="nav nav-pills nav-justified">
+		<li><a href="Workerlandingpage.php">Home</a></li>
 		<li><a href="AddNewChild.php">Add Child</a></li>
-		<li><a href="AddNewWorker.php">Add Employee</a></li>
+		<li class="active"><a href="AddNewWorker.php">Add Employee</a></li>
 		<li><a href="PasswordReset.php">Password Reset</a></li>
-	</ul>	
-	<div id="names" style="width: 200px;background-color: #FFFFCC; float: left;">
-	<form action="AddNewWorker.php" method="get">
-		<input type="submit" value="Add New" id="new">
-		
-		
-	</form>
-		<table rules="rows" width="200px">
+	</ul>
+	<div class="table-responsive" id="names" style="width: 200px;background-color: #1a1a1a; float: left;">
+			<table class="table" width="200px">
 			<tr>
-				<th><center>Name</center></th>
+				<th><center>
+				<h4>Employee Roster</h4>
+				<a href="AddNewWorker.php" class="btn btn-primary" role="button">Add New</a>
+				<br />
+				</th>
+			</tr>	
 				
 <?php
 $server = "localhost";
@@ -105,54 +125,61 @@ if (empty($EmpKey) && empty($result)){
 		echo "<meta http-equiv='refresh' content='0'>";
 	}
 ?>
-	<div id="topPage" style="margin-left: 200px; background-color: #CCFFFF;" valign="top">
-	<b>Please Add a new employee, or select a employee to Edit/Delete</b>
-	<p></p>
+	<div class="table-responsive" id="topPage" style="margin-left: 200px; background-color: #006600;" valign="top">
+	<h5><center>Please Add a new employee, or select one to Edit/Delete</center></h5>
+	
 	
 	 <form method="post">
+	 
+			<div class="container-fluid">
+			<div class="row table-responsive">
+			<div class="col-xs-7">
 			
-			<div class="left">
-				<label for="name">Employee First Name</label>
+			<div class="col-sm-4">
+				<label for="name">First Name</label>
 			</div>
-			<div class "right">
-				<input type = "text" name= "fName" size="16" value="">
+			<div class="col-sm-8">
+				<input type = "text" name= "fName" size="25" value="">
 			</div>
 			
-			<div class="left">
-				<label for="name">Employee Last Name</label>
+			<div class="col-sm-4">
+				<label for="name">Last Name</label>
 			</div>
-			<div class "right">
-				<input type = "text" name= "lName" size="16" value="">
+			<div class="col-sm-8">
+				<input type = "text" name= "lName" size="25" value="">
 			</div>
             
-			<div class="left">
-				<label for="position">Employee Position</label>
+			<div class="col-sm-4">
+				<label for="position">Position</label>
 			</div>
-			<div class "right">
-				<input type = "text" name= "position" size="16" value="">
+			<div class="col-sm-8">
+				<input type = "text" name= "position" size="25" value="">
 			</div>
-			<div class="left">
-				<label for="hireDate">Employee Hire Date</label>
+			<div class="col-sm-4">
+				<label for="hireDate">Hire Date</label>
 			</div>
-			<div class "right">
-				<input type = "text" name= "hireDate" size="16" value="">
+			<div class="col-sm-8">
+				<input type = "text" name= "hireDate" size="10" value="">
 			</div>
-			<div class="left">
-				<label for="phoneNum">Employee Phone Number</label>
+			<div class="col-sm-4">
+				<label for="phoneNum">Phone Number</label>
 			</div>
-			<div class "right">
+			<div class="col-sm-8">
 				<input type = "text" name= "empPhoneNum" size="16" value="">
 			</div>
 			
+		</div>
+			
+		<div class="col-xs-5"><br /><br /><img src="img/wecare.png" class="img-rounded" alt="daycare-image" width="85%" height="auto"></div>
+		</div>
+			
 			<div class="left">
-				<button type="submit" name="save">Save</button>
+				<button type="submit" class="btn btn-success" name="save">Save New Employee</button>
 			</div>
 		</form>
-		<form action="ChildDailyAct.php">
-			<div class="right">
-				<button type="submit" name="back">Back</button>
-			</div>
-		</form>
+		</div>
+		</div>
+						
 <?php
 }
 else{
@@ -186,54 +213,68 @@ while($EmpInfo = mysqli_fetch_assoc($result))
 			echo "<meta http-equiv='refresh' content='0'>";
 		}
 ?>
-<div id="topPage" style="margin-left: 200px; background-color: #CCFFFF;" valign="top">			
+<div class="table-responsive" id="topPage" style="margin-left: 200px; background-color: #006600;" valign="top">	
 	<form method="post">
+		
+		<div class="container-fluid">
+			<div class="row table-responsive">
+			<div class="col-xs-7">
 			
-			<div class="left">
-				<label for="name">Employee First Name</label>
+			<div class="col-sm-4">
+				<label for="name">First Name</label>
 			</div>
-			<div class "right">
-				<?php echo '<input type = "text" name= "fName" size="16" value="' . $EmpInfo['First_Name'] . '">'?>
+			<div class="col-sm-8">
+				<?php echo '<input type = "text" name= "fName" size="25" value="' . $EmpInfo['First_Name'] . '">'?>
 			</div>
 			
-			<div class="left">
-				<label for="name">Employee Last Name</label>
+			<div class="col-sm-4">
+				<label for="name">Last Name</label>
 			</div>
-			<div class "right">
-				<?php echo '<input type = "text" name= "lName" size="16" value="' . $EmpInfo['Last_Name'] . '">'?>
+			<div class="col-sm-8">
+				<?php echo '<input type = "text" name= "lName" size="25" value="' . $EmpInfo['Last_Name'] . '">'?>
 			</div>
             
-			<div class="left">
-				<label for="position">Employee Position</label>
+			<div class="col-sm-4">
+				<label for="position">Position</label>
 			</div>
-			<div class "right">
-				<?php echo '<input type = "text" name= "position" size="16" value="' . $EmpInfo['Position'] . '">'?>
+			<div class="col-sm-8">
+				<?php echo '<input type = "text" name= "position" size="25" value="' . $EmpInfo['Position'] . '">'?>
 			</div>
-			<div class="left">
-				<label for="hireDate">Employee Hire Date</label>
+			<div class="col-sm-4">
+				<label for="hireDate">Hire Date</label>
 			</div>
-			<div class "right">
-				<?php echo '<input type = "text" name= "hireDate" size="16" value="' . $EmpInfo['Hire_Date'] . '">'?>
+			<div class="col-sm-8">
+				<?php echo '<input type = "text" name= "hireDate" size="10" value="' . $EmpInfo['Hire_Date'] . '">'?>
 			</div>
-			<div class="left">
-				<label for="phoneNum">Employee Phone Number</label>
+			<div class="col-sm-4">
+				<label for="phoneNum">Phone Number</label>
 			</div>
-			<div class "right">
+			<div class="col-sm-8">
 				<?php echo '<input type = "text" name= "empPhoneNum" size="16" value="' . $EmpInfo['Phone_Num'] . '">'?>
 			</div>		
 		     
-			 <div class="left">
+			 
+		</div>
+			
+		<div class="col-xs-5"><br /><br /><img src="img/wecare.png" class="img-rounded" alt="daycare-image" width="85%" height="auto"></div>
 		
-		<button type="submit" name="update">Update</button>
+		</div>
+		
+		<div class="left">
+		<button type="submit" class="btn btn-success" name="update">Update</button>
+		</div>
 		
 		
-		
-		</div> <div class="right">
-		<button type="submit" name="delete" onclick="return deleteCheck()" >Delete</button>
+		 <div class="right">
+		<button type="submit" class="btn btn-danger" name="delete" onclick="return deleteCheck()" >Delete</button>
 	
+		
+		</div>
 		
 		
 		</form>
+		</div>
+		</div>
 <?php ;}
 }
 ?>
